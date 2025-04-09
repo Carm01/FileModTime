8e31264bcc832a60b4e00d3ba53cb5170e67fc0d
Carm01 <6567665+Carm01@users.noreply.github.com>
2025-04-09 08:42:35 -0400

    Update findALLFIlesMod.ps1

--- findALLFIlesMod.ps1
+++ findALLFIlesMod.ps1
@@ -1,8 +1,12 @@
 # Set the directory to search (change this to the desired directory)
 $directory = "\\your\file\path"
 
+$OutputDirectory = "./" # Set the output directory (change this to the desired output directory) - Default is current directory
+
 # Set the output CSV file name
-$outputFile = "output.csv"
+<# $outputFile = "output.csv" #>
+$outputFile = ("FileLookUp_" + $(([System.Uri]$directory).host) + "_" + $(Get-Date -Format yyyyMMdd) + ".csv")
+$OutputFile_FullPath = Join-Path -Path $OutputDirectory -ChildPath $outputFile
 
 # Ensure the directory exists
 if (-Not (Test-Path -Path $directory)) {
@@ -17,12 +21,13 @@ $files = Get-ChildItem -Path $directory -Recurse -File -ErrorAction SilentlyCont
 $sortedFiles = $files | Sort-Object LastWriteTime -Descending
 
 # Prepare CSV output (Exclude file name from full path)
-$sortedFiles | Select-Object @{Name="Last Modified Time";Expression={$_.LastWriteTime}}, 
+$sortedFiles | Select-Object @{Name="File Create Time";Expression={$_.CreationTime}},
+							@{Name="Last Modified Time";Expression={$_.LastWriteTime}},
                            @{Name="Full File Path";Expression={$_.DirectoryName}}, 
                            @{Name="File Name";Expression={$_.Name}} | 
-    Export-Csv -Path $outputFile -NoTypeInformation
+    Export-Csv -Path $OutputFile_FullPath -NoTypeInformation
 
-Write-Host "CSV file has been generated: $outputFile"
+Write-Host "CSV file has been generated: $OutputFile_FullPath"
 
 # Close the PowerShell window after completion
 exit
